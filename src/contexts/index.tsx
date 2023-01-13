0cc9a021f71b071e6516013a9f2df7365e9b7cb8
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-01-13 19:30:18 -0300

    feat(Providers): add AuthContexProvider

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,16 +1,17 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 import { ThemeProvider } from "./ThemeContext";
+import { AuthContextProvider } from "./auth";
 
 interface ProviderProp {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Providers = ({ children }: ProviderProp) => {
-    return (
-        <ThemeProvider>
-            {children}
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
+    </ThemeProvider>
+  );
+};
 
 export default Providers;
