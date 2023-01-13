df3ff8b6c233ea905745e4b71192ff704a6016db
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-13 02:31:30 -0300

    style(HeaderModal): applied transition initial

--- src/components/HeaderModal/styles.ts
+++ src/components/HeaderModal/styles.ts
@@ -4,7 +4,28 @@ export const Container = styled.section`
   height: 100vh;
   width: 100vw;
   background-color: #fff;
-  border: solid 1px #000;
   position: fixed;
   z-index: 15;
-`;
+  animation: go-back 1s;
+  animation-direction: alternate-reverse;
+
+  @keyframes go-back {
+    from {
+      transform: translateX(100vw);
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+
+    img {
+      height: 35px;
+    }
+  }
+`; 
