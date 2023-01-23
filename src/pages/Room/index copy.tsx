4cda2f730c6f8b441281c1d3bec713e272aafa98
Bruno Sallum <brusallum@hotmail.com>
2023-01-23 02:28:19 -0300

    fix(room): revert

---
+++ src/pages/Room/index copy.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import api from "services/api";
+import { useParams } from "react-router";
+import { useAuth } from "contexts/auth";
+import Login from "pages/Login";
+
+const Room = () => {
+  const { roomId } = useParams();
+  const { needed, setNeeded, logged, logout } = useAuth();
+
+  useEffect(() => {
+    if (logged) {
+      api.get(`room/connect/${roomId}`)
+      .then(res => {
+        console.log("🚀 ~ file: index.tsx:23 ~ Room ~ res", res)
+      })
+      setNeeded(false)
+    } else {
+      setNeeded(true)
+    }
+  })
+  
+  return (
+    needed ? <Login /> :
+    <>
+      <div>
+        <h2>
+          <button onClick={logout}>
+            logout
+          </button>
+          Hello
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default Room;
