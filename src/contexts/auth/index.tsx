2fef506a0b7135158c17393eb752b979de816748
Bruno Sallum <brusallum@hotmail.com>
2023-01-23 20:38:24 -0300

    feat(room): login refactor

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -47,7 +47,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       setLogged(true);
       setNeeded(false);
       setRole(res.data.user.role);
-      navigate("/");
+      // navigate("/");
     })
     .finally(() => {
       //mensagem de login bem sucedido
