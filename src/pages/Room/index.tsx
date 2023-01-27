c6924b1a300f59ceba69002c176893d96fe9f5be
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-26 23:55:56 -0300

    feat(pages): new config Room

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import "./styles";
+import React, { useState } from "react";
+import { MeetingProvider, MeetingConsumer } from "@videosdk.live/react-sdk";
+import { authToken, CreateMeeting } from "../../api";
+import JoinScreen from "../../components/Room/JoinScreen";
+import Container from "../../components/Room/Container";
 
 const Room = () => {
-  return (
-    <div>
-      <h2>Olá, eu sou a Room :)</h2>
-    </div>
-  )
-}
+  const [meetingId, setMeetingId] = useState<string>('');
+
+  const getMeetingAndToken = async (id: string) => {
+    const meetingId =
+      id == null ? await CreateMeeting({ token: authToken }) : id;
+    setMeetingId(meetingId);
+  };
+
+  return authToken && meetingId ? (
+    <MeetingProvider
+      config={{
+        meetingId,
+        micEnabled: true,
+        webcamEnabled: true,
+        name: "C.V. Raman",
+      }}
+      token={authToken}
+    >
+      <MeetingConsumer>
+        {() => <Container meetingId={meetingId} />}
+      </MeetingConsumer>
+    </MeetingProvider>
+  ) : (
+    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
+  );
+};
 
 export default Room;
