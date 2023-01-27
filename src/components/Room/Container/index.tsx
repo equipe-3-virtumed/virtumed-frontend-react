a2683ac8b1b1792fb49924378d49beae6b52c789
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-26 23:57:01 -0300

    feat(Room): create & config

---
+++ src/components/Room/Container/index.tsx
@@ -0,0 +1,32 @@
+import { useMeeting } from "@videosdk.live/react-sdk";
+import { useState } from "react";
+import Controls from "../Controls";
+import VideoComponent from "../VideoComponent";
+
+const Container = (props: { meetingId: string }) => {
+  const [joined, setJoined] = useState(false);
+  const { join } = useMeeting();
+  const { participants } = useMeeting();
+  const joinMeeting = () => {
+    setJoined(true);
+    join();
+  };
+
+  return (
+    <div className="container">
+      <h3>Meeting Id: {props.meetingId}</h3>
+      {joined ? (
+        <div>
+          <Controls />
+          {[...participants.keys()].map((participantId) => (
+            <VideoComponent participantId={participantId} />
+          ))}
+        </div>
+      ) : (
+        <button onClick={joinMeeting}>Join</button>
+      )}
+    </div>
+  );
+};
+
+export default Container;
