c1e2ff3227e72c194562eebe66f4204f7f1334ef
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-27 00:00:18 -0300

    feat(Room): create & config VideoComponent

---
+++ src/components/Room/VideoComponent/index.tsx
@@ -0,0 +1,63 @@
+import { useParticipant } from "@videosdk.live/react-sdk";
+import { useEffect, useMemo, useRef } from "react";
+import ReactPlayer from "react-player";
+
+const VideoComponent = (props: any) => {
+  const micRef: any = useRef(null);
+  const { webcamStream, micStream, webcamOn, micOn, isLocal } = useParticipant(
+    props.participantId
+  );
+
+  const videoStream = useMemo(() => {
+    if (webcamOn) {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(webcamStream);
+      return mediaStream;
+    }
+  }, [webcamStream, webcamOn]);
+
+  useEffect(() => {
+    if (micRef.current) {
+      if (micOn) {
+        const mediaStream = new MediaStream();
+        mediaStream.addTrack(micStream);
+
+        micRef.current.srcObject = mediaStream;
+        micRef.current
+          .play()
+          .catch((error: any) =>
+            console.error("videoElem.current.play() failed", error)
+          );
+      } else {
+        micRef.current.srcObject = null;
+      }
+    }
+  }, [micStream, micOn]);
+
+  return (
+    <div key={props.participantId}>
+      {micOn && micRef && <audio ref={micRef} autoPlay muted={isLocal} />}
+      {webcamOn && (
+        <ReactPlayer
+          //
+          playsinline // very very imp prop
+          pip={false}
+          light={false}
+          controls={true}
+          muted={true}
+          playing={true}
+          //
+          url={videoStream}
+          //
+          height={"100px"}
+          width={"100px"}
+          onError={(err) => {
+            console.log(err, "participant video error");
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoComponent;
