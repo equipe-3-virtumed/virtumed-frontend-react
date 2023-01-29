35df32fc762fdb970df970eeef7e89c9d491b88f
Bruno Sallum <brusallum@hotmail.com>
2023-01-29 17:58:57 -0300

    fix(home): align Container

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-	display: flex;
+	/* display: flex;
 	flex-direction: column;
 	width: 100vw;
-	height: 100vh;
+	height: 100vh; */
 
 	p {
 		text-align: center;
