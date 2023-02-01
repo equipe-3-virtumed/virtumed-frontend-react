5cde01fd75f4819e98fa348e16fbfbb0c3124fa9
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-02-01 15:46:22 -0300

    remove(Container): Controls component

--- src/components/Room/Container/index.tsx
+++ src/components/Room/Container/index.tsx
@@ -1,12 +1,10 @@
 import { useMeeting } from "@videosdk.live/react-sdk";
 import { useState } from "react";
-import Controls from "../Controls";
 import VideoComponent from "../VideoComponent";
 
 const Container = (props: { meetingId: string }) => {
   const [joined, setJoined] = useState(false);
   const { join, participants } = useMeeting();
-  // const { participants } = useMeeting();
   const joinMeeting = () => {
     setJoined(true);
     join();
@@ -17,7 +15,6 @@ const Container = (props: { meetingId: string }) => {
       <h3>Meeting Id: {props.meetingId}</h3>
       {joined ? (
         <div>
-          <Controls />
           {[...participants.keys()].map((participantId) => (
             <VideoComponent participantId={participantId} />
           ))}
