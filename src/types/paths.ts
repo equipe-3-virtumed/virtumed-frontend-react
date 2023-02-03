56ab59b3590170a9ec3590083816bb695e9c1080
Bruno Sallum <brusallum@hotmail.com>
2023-02-03 05:45:47 -0300

    update(merge): button

--- src/types/paths.ts
+++ src/types/paths.ts
@@ -4,7 +4,7 @@ export enum RoutesPath {
   REGISTER = "/register",
   PATIENT_PAGE = "/patient",
   DOCTOR_PAGE = "/doctor",
-  CLINIC_PAGE = "n/clinic",
+  CLINIC_PAGE = "/clinic",
   ROOM_PAGE = "/consulta/:roomId",
   CONNECT_ROOM_PAGE = "/consulta/entrar/:roomId"
 }
