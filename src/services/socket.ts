2e050cbb099e780d6f654bf3a08caa42e63d3bfa
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-02-03 16:22:52 -0300

    refactor change links position

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 
 const socket = io(
-  "http://localhost:3333/chat" ||
-  "virtumed-backend-production.up.railway.app/chat"
+  "virtumed-backend-production.up.railway.app/chat" ||
+    "http://localhost:3333/chat"
 );
 
 export default socket;
