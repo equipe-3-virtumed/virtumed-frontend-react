1857688572ab3b7fbeb3ffd41a4f7fae821a020c
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-02-03 18:11:08 -0300

    fix: links socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 
 const socket = io(
-  "https://virtumed-backend-production.up.railway.app/" ||
-    "http://localhost:3333/"
+  "https://virtumed-backend-production.up.railway.app/chat" ||
+    "http://localhost:3333/chat"
 );
 
 export default socket;
